37ef2e844addaeac6037736426c3ce89642602b5
donghao <2535615874@qq.com>
2020-11-10 15:08:51 +0800

    fix pm2 info

--- api/article.js
+++ api/article.js
@@ -2,25 +2,25 @@ import { get } from '../utils/request'
 import config from '../config/index'
 
 export const getArticleList = params =>
-  get(`${config.BASE_URL}api/blog/v1/article/list`, params)
+  get(`api/blog/v1/article/list`, params)
 
 /**
    * 获取最近文章
    * @param {*} params
    */
 export const getLatestArticle = params =>
-  get(`${config.BASE_URL}api/blog/v1/article/latest`, params)
+  get(`api/blog/v1/article/latest`, params)
 
 /**
    * 获取热门文章
    * @param {} params
    */
 export const getHotArticle = params =>
-  get(`${config.BASE_URL}api/blog/v1/article/hot`, params)
+  get(`api/blog/v1/article/hot`, params)
 
 /**
    * 获取相关文章
    * @param {*} params
    */
 export const getRelevantArticle = params =>
-  get(`${config.BASE_URL}api/blog/v1/article/relevant`, params)
+  get(`api/blog/v1/article/relevant`, params)
