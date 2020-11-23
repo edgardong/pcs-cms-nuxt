e1529fd5694a33947be2702bd94563b5c3656707
donghao <2535615874@qq.com>
2020-11-23 17:41:13 +0800

    添加.nuxt文件

---
+++ .nuxt/App.js
@@ -0,0 +1,161 @@
+import Vue from 'vue'
+import { getMatchedComponentsInstances, promisify, globalHandleError } from './utils'
+import NuxtLoading from './components/nuxt-loading.vue'
+
+import '../node_modules/ant-design-vue/dist/antd.css'
+
+import '../assets/web.css'
+
+import _cb805f78 from '../layouts/blog-content.vue'
+import _275fa447 from '../layouts/blog-feeling.vue'
+import _bcd7bd3c from '../layouts/blog-header.vue'
+import _4749fb62 from '../layouts/blog-pages.vue'
+import _6f6c098b from '../layouts/default.vue'
+
+const layouts = { "_blog-content": _cb805f78,"_blog-feeling": _275fa447,"_blog-header": _bcd7bd3c,"_blog-pages": _4749fb62,"_default": _6f6c098b }
+
+export default {
+  head: {"title":"wecblog-vue","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"前沿技术网，专注于探寻web前端开发技术。探索和分享各种web前端及前端开发中的技术，解决web前端开发中遇到的各种问题，努力为大家打造一个良好的web前端开发的技术交流探讨及分享平台。"}],"script":[{"type":"text\u002Fjavascript","src":"https:\u002F\u002Fcdn.jsdelivr.net\u002Fgh\u002Fgoogle\u002Fcode-prettify@master\u002Floader\u002Frun_prettify.js?lang=css&amp;skin=sunburst","async":true}],"link":[],"style":[]},
+
+  render(h, props) {
+    const loadingEl = h('NuxtLoading', { ref: 'loading' })
+    const layoutEl = h(this.layout || 'nuxt')
+    const templateEl = h('div', {
+      domProps: {
+        id: '__layout'
+      },
+      key: this.layoutName
+    }, [ layoutEl ])
+
+    const transitionEl = h('transition', {
+      props: {
+        name: 'layout',
+        mode: 'out-in'
+      },
+      on: {
+        beforeEnter(el) {
+          // Ensure to trigger scroll event after calling scrollBehavior
+          window.$nuxt.$nextTick(() => {
+            window.$nuxt.$emit('triggerScroll')
+          })
+        }
+      }
+    }, [ templateEl ])
+
+    return h('div', {
+      domProps: {
+        id: '__nuxt'
+      }
+    }, [loadingEl, transitionEl])
+  },
+  data: () => ({
+    isOnline: true,
+    layout: null,
+    layoutName: ''
+  }),
+  beforeCreate() {
+    Vue.util.defineReactive(this, 'nuxt', this.$options.nuxt)
+  },
+  created() {
+    // Add this.$nuxt in child instances
+    Vue.prototype.$nuxt = this
+    // add to window so we can listen when ready
+    if (process.client) {
+      window.$nuxt = this
+      this.refreshOnlineStatus()
+      // Setup the listeners
+      window.addEventListener('online', this.refreshOnlineStatus)
+      window.addEventListener('offline', this.refreshOnlineStatus)
+    }
+    // Add $nuxt.error()
+    this.error = this.nuxt.error
+    // Add $nuxt.context
+    this.context = this.$options.context
+  },
+
+  mounted() {
+    this.$loading = this.$refs.loading
+  },
+  watch: {
+    'nuxt.err': 'errorChanged'
+  },
+
+  computed: {
+    isOffline() {
+      return !this.isOnline
+    }
+  },
+  methods: {
+    refreshOnlineStatus() {
+      if (process.client) {
+        if (typeof window.navigator.onLine === 'undefined') {
+          // If the browser doesn't support connection status reports
+          // assume that we are online because most apps' only react
+          // when they now that the connection has been interrupted
+          this.isOnline = true
+        } else {
+          this.isOnline = window.navigator.onLine
+        }
+      }
+    },
+    async refresh() {
+      const pages = getMatchedComponentsInstances(this.$route)
+
+      if (!pages.length) {
+        return
+      }
+      this.$loading.start()
+      const promises = pages.map(async (page) => {
+        const p = []
+
+        if (page.$options.fetch) {
+          p.push(promisify(page.$options.fetch, this.context))
+        }
+        if (page.$options.asyncData) {
+          p.push(
+            promisify(page.$options.asyncData, this.context)
+              .then((newData) => {
+                for (const key in newData) {
+                  Vue.set(page.$data, key, newData[key])
+                }
+              })
+          )
+        }
+        return Promise.all(p)
+      })
+      try {
+        await Promise.all(promises)
+      } catch (error) {
+        this.$loading.fail()
+        globalHandleError(error)
+        this.error(error)
+      }
+      this.$loading.finish()
+    },
+
+    errorChanged() {
+      if (this.nuxt.err && this.$loading) {
+        if (this.$loading.fail) this.$loading.fail()
+        if (this.$loading.finish) this.$loading.finish()
+      }
+    },
+
+    setLayout(layout) {
+      if (!layout || !layouts['_' + layout]) {
+        layout = 'default'
+      }
+      this.layoutName = layout
+      this.layout = layouts['_' + layout]
+      return this.layout
+    },
+    loadLayout(layout) {
+      if (!layout || !layouts['_' + layout]) {
+        layout = 'default'
+      }
+      return Promise.resolve(layouts['_' + layout])
+    }
+  },
+  components: {
+    NuxtLoading
+  }
+}
---
+++ .nuxt/router.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _ff7e4858 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
+const _0c9e081b = () => interopDefault(import('../pages/test.vue' /* webpackChunkName: "pages/test" */))
+const _00e63fb0 = () => interopDefault(import('../pages/article/_id.vue' /* webpackChunkName: "pages/article/_id" */))
+const _c00e92ce = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _2b458b41 = () => interopDefault(import('../pages/_id.vue' /* webpackChunkName: "pages/_id" */))
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+      path: "/about",
+      component: _ff7e4858,
+      name: "about"
+    }, {
+      path: "/test",
+      component: _0c9e081b,
+      name: "test"
+    }, {
+      path: "/article/:id?",
+      component: _00e63fb0,
+      name: "article-id"
+    }, {
+      path: "/",
+      component: _c00e92ce,
+      name: "index"
+    }, {
+      path: "/:id",
+      component: _2b458b41,
+      name: "id"
+    }],
+
+  fallback: false
+}
+
+export function createRouter() {
+  return new Router(routerOptions)
+}
